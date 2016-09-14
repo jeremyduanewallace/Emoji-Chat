2baa114d4ef4fefe75838260f68443d34a0cad57
jeremyduanewallace <jeremyduanewallace@gmail.com>
2016-09-13 18:37:40 -0700

    redis url

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 
 module Campfire
   class Application < Rails::Application
+    configaction_cable.mount_path = '/websocket'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
